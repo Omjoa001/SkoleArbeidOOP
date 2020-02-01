10bc6366c9ef0dac24eec19318a65421b0bde212
Omjoa001 <omerjonuzi7@gmail.com>
2020-02-01 17:19:05 +0100

    oppgave2

--- Oving4.js
+++ Oving4.js
@@ -1,11 +1,51 @@
-let tall = 6
+/*
+Oppgave 1:
+let tall = () => {
+  return (Math.random()*20)
+}
+ let p = new Promise((resolve,reject) => {
+   let t = tall()
+   if (t >= 10) {
+     resolve(t + " er større enn 10")
+   } else {
+     let feilmelding = new Error("Tallet er ikke stor nok: ")
+     reject(feilmelding + t)
+   }
+ })
 
-let firstPromise = new Promise(function tesTall(resolve,reject) {
-if (tall > 10) {
-resolve(tall)
-} else {
-  let grunn =  new Error("Tallet er ikke stor nok")
-  reject(error.message)
+ p.then(verdi => {
+   console.log(verdi);
+ }).catch(feil => {
+   console.log(feil);
+ })
+*/
+let ord = ["a", "B", "c", "D", "e", "F"];
+
+let o = () => {
+  return new Promise((resolve, reject) => {
+    let ingenTall = true;
+
+    let i = () => {
+      ord.forEach(item => {
+        if (typeof item != "string") {
+          ingenTall = false;
+        }
+      });
+    };
+
+    if (ingenTall) {
+      ord.forEach(item => {
+        item.toLowerCase();
+      });
+      resolve(ord);
     }
-  }
-)
+  });
+};
+
+o()
+  .then(verdi => {
+    console.log(verdi);
+  })
+  .catch(feil => {
+    console.log(feil);
+  });

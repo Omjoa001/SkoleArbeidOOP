abc6375f4bc649bd3b6353d4b74f1b36af302884
Omjoa001 <omerjonuzi7@gmail.com>
2020-02-05 23:05:13 +0100

    adda key

--- Oving5.js
+++ Oving5.js
@@ -1,10 +1,22 @@
-let tabellHTML = "<table style='border: 1px solid black'>"
+let tabellHTML = "<table style='border: 1px solid black'>";
 
-$.getJSON("https://api.myjson.com/bins/rj5ai", (data) =>{
-data.fjelltopper.sort((a, b) => (a.høyde > b.høyde) ? 1 : -1)
+$.getJSON("https://api.myjson.com/bins/rj5ai", data => {
+  data.fjelltopper.sort((a, b) => (a.høyde > b.høyde ? 1 : -1));
   for (var i = 0; i < data.fjelltopper.length; i++) {
-    tabellHTML += `<tr> <th> ${data.fjelltopper[i].fylke} </th></tr><td> ${data.fjelltopper[i].fjell} </td> <td> ${data.fjelltopper[i].høyde} </td>`
+    tabellHTML += `<tr> <th> ${data.fjelltopper[i].fylke} </th></tr><td> ${data.fjelltopper[i].fjell} </td> <td> ${data.fjelltopper[i].høyde} </td>`;
   }
-  tabellHTML += "</table>"
-  document.getElementById('utdata').innerHTML = tabellHTML
-    });//bruker http://myjson.com/rj5ai for å lagre json filen
+  tabellHTML += "</table>";
+  document.getElementById("utdata").innerHTML = tabellHTML;
+}); //bruker http://myjson.com/rj5ai for å lagre json filen
+
+function initMap() {
+  // The location of Uluru
+  var uluru = { lat: -25.344, lng: 131.036 };
+  // The map, centered at Uluru
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru
+  });
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({ position: uluru, map: map });
+}

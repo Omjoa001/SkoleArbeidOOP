644428849d1d1fb97f75fc5b31a1c74af5c2d3d2
Omjoa001 <omerjonuzi7@gmail.com>
2020-02-07 00:10:01 +0100

    n

--- Oving5.js
+++ Oving5.js
@@ -1,23 +1,45 @@
-let tabellHTML = "<table style='border: 1px solid black'>";
-
 $.getJSON("https://api.myjson.com/bins/dbsca", data => {
   data.fjelltopper.sort((a, b) => (a.høyde > b.høyde ? 1 : -1));
   for (var i = 0; i < data.fjelltopper.length; i++) {
-    tabellHTML += `<tr> <th> ${data.fjelltopper[i].fylke} </th></tr><td> ${data.fjelltopper[i].fjell} </td> <td> ${data.fjelltopper[i].høyde} </td>`;
+    let rad = document.createElement("tr")
+
+    let hode = document.createElement("th")
+    let hodeTekst = document.createTextNode(data.fjelltopper[i].fylke)
+    hode.appendChild(hodeTekst)
+    rad.appendChild(hode)
+
+    let innhold = document.createElement("td")
+    let innholdFjell = document.createTextNode(data.fjelltopper[i].fjell + ": " + data.fjelltopper[i].høyde + "m")
+    innhold.appendChild(innholdFjell)
+    rad.appendChild(innhold)
+    rad.setAttribute("onclick",visTopp(data.fjelltopper[i].lat,data.fjelltopper[i].lng))
+
+
+    document.getElementById("tabellHTML").appendChild(rad)
   }
-  tabellHTML += "</table>";
-  document.getElementById("utdata").innerHTML = tabellHTML;
 }); //bruker http://myjson.com/rj5ai for å lagre json filen
+let markertFjell = [{lat: 60.45038, lng: 10.949903 },{ lat: 59.767205, lng: 7.116667 }]
+
+let visTopp = (x,y) => {
+let fjellObjekt = {
+  lat: x,
+  lng: y
+}
+markertFjell.push(fjellObjekt)
+initMap()
+}
 
 function initMap() {
-  // The location of Uluru
+  // The location of fjelltopp
   var fjellet = { lat: 60.45038, lng: 10.949903 };
   var fjellet2 = { lat: 59.767205, lng: 7.116667 };
-  // The map, centered at Uluru
+  // The map, centered at fjell
   var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
     center: fjellet
   });
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({ position: fjellet, map: map });
+  // The marker, positioned at fjell
+  for (var i = 0; i < markertFjell.length; i++) {
+    let marker = new google.maps.Marker({ position: markertFjell[i], map: map });
+  }
 }

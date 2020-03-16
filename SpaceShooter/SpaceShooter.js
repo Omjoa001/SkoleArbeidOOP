718a2e881d16d945969c53993eca07c580a3a2d3
Omjoa001 <omerjonuzi7@gmail.com>
2020-03-16 23:02:32 +0100

    lager sjekk for treff

--- SpaceShooter/SpaceShooter.js
+++ SpaceShooter/SpaceShooter.js
@@ -60,23 +60,30 @@ let flyttLaser = x => {
       teller += 2;
       //dersom man endrer på hvor mye teller plusses med går laser fortere.
       x.style.bottom = teller + "px";
+      sjekkTreff(x); //sender denne laseren som paramter i funksjonen som sjekker treff
     } else {
       x.parentElement.removeChild(x); //removeChild() fungerer for å fjerne elementet. Må alltid kalle gjennom parent for å bruke den
-      clearInterval(bevegelsen);
+      clearInterval(bevegelsen); //slutter å kjøre flytt laser funksjonen ettersom det ikke eksisterer noen laser å flytte på lenger.
     }
   }, 1); //kan også endre på hvor ofte teller oppdateres. satt på minst mulig verdi her siden det ser mer smooth ut (bare å teste selv)
 };
 //Må prøve å fjerne laserne før jeg kan gå videre på å lage UFOs for å forsikre om at jeg har kontroll på den enheten.
 
+let ufoListe = []; //lagrer alle ufos her. Alle lasere sjekker om de treffer ufoene hver gang de flytter på seg
+
 let lagUFO = setInterval(() => {
   //gjenbruker lignende kode som for lagLaser
   let ufo = document.createElement("div");
   ufo.setAttribute("class", "ufoClass");
-  let plassering =
-    Math.random() * 30 +
-    parseInt(getComputedStyle(spillBoks).getPropertyValue("width")); // plusser med 30 som er bredden til ufo sånn at det ikke ser ut som at den går ut av boksen
+  let plassering = Math.floor(
+    Math.random() *
+      (parseInt(getComputedStyle(spillBoks).getPropertyValue("width")) - 80) +
+      30
+  ); // plusser med 30 som er bredden til ufo sånn at det ikke ser ut som at den går ut av boksen
+  //(Math.random() * (max - min) ) + min = Formelen for å få en math random verdi med minimum og maksimum verdi.
   ufo.setAttribute("style", "left:" + plassering + "px");
   document.getElementById("spillBoks").appendChild(ufo);
+  ufoListe.push(ufo);
   flyttUfo(ufo);
 }, 5000);
 
@@ -85,15 +92,35 @@ let flyttUfo = x => {
   let teller = parseInt(getComputedStyle(x).getPropertyValue("bottom"));
   console.log(teller);
   let bevegelsen = setInterval(() => {
-    if (teller >= 0) {
+    if (
+      teller >=
+      parseInt(getComputedStyle(romskip).getPropertyValue("bottom")) +
+        parseInt(getComputedStyle(romskip).getPropertyValue("height"))
+    ) {
       console.log("lagd");
       teller -= 2;
       //dersom man endrer på hvor mye teller plusses med går laser fortere.
       x.style.bottom = teller + "px";
     } else {
       console.log("fjernet");
+      let i = ufoListe.indexOf(x); //finner posisjonen til elementet i arrayen
+      ufoListe.splice(i, 1); //fjernen elementet fra arrayen. Neste linje sletter selve elementet.
       x.parentElement.removeChild(x); //removeChild() fungerer for å fjerne elementet. Må alltid kalle gjennom parent for å bruke den
       clearInterval(bevegelsen);
     }
   }, 10); //kan
+};
+
+let sjekkTreff = laser => {
+  for (var i = 0; i < ufoListe.length; i++) {
+    let x = parseInt(getComputedStyle(ufoListe[i]).getPropertyValue("left"));
+    if (x == parseInt(getComputedStyle(laser).getPropertyValue("left"))) {
+      let y = parseInt(
+        getComputedStyle(ufoListe[i]).getPropertyValue("bottom")
+      );
+      if (y == parseInt(getComputedStyle(laser).getPropertyValue("bottom"))) {
+        treff();
+      }
+    }
+  }
 };
